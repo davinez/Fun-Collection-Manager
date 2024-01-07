ea1ea13551cc41796dba7997ed0cefc332c5a23e
david.narvaez <david.ibanezn@gmail.com>
2024-01-06 22:45:27 -0600

    chore: set initial event handlers for header filters of collections

--- src/WebSPA/src/routes/manager/AllBookmarksPage.tsx
+++ src/WebSPA/src/routes/manager/AllBookmarksPage.tsx
@@ -6,37 +6,29 @@ import {
 	Box,
 	Flex,
 	Icon,
-	CheckboxGroup,
-	Checkbox,
+	Image,
 	Button,
 	Popover,
 	PopoverTrigger,
 	PopoverContent,
-	PopoverHeader,
 	PopoverBody,
-	PopoverFooter,
-	PopoverArrow,
-	PopoverCloseButton,
-	PopoverAnchor,
 	RadioGroup,
 	Radio,
+	Checkbox,
+	CheckboxGroup,
+	useCheckboxGroup,
+	Divider,
 	type FlexProps,
 } from "@chakra-ui/react";
 import {
-	AiFillCaretDown,
-	AiFillCaretRight,
-	AiFillFolder,
-	AiOutlineUser,
 	AiFillCloud,
-	AiFillDelete,
-	AiFillSetting,
-	AiOutlineLogout,
-	//
 	AiFillClockCircle,
 	AiFillChrome,
+	AiFillProfile,
+	AiFillLayout,
+	AiFillAppstore,
 } from "react-icons/ai";
 import { FaArrowDownAZ, FaArrowUpAZ } from "react-icons/fa6";
-import textStylesTheme from "shared/styles/theme/foundations/textStyles";
 // Components
 
 // Assets
@@ -45,25 +37,152 @@ import textStylesTheme from "shared/styles/theme/foundations/textStyles";
 import { useGetCollectionsQuery } from "@/api/services/manager";
 // Types
 import type { TCollection } from "@/shared/types/api/manager.types";
-import { FormActionEnum } from "@/shared/types/global.types";
+import {
+	EventOrValue,
+	SortEnum,
+	ViewCollectionsEnum,
+	ShowInBookmarkEnum,
+} from "@/shared/types/global.types";
 // General
 import { Fragment, useState, useEffect } from "react";
 import { useStore } from "@/store/UseStore";
 import { defaultHandlerApiError } from "@/api/apiClient";
 
 type THeaderFiltersProps = {
-	icon?: string | React.ElementType; // Third party icon
+	icon: string | React.ElementType; // Third party icon
 	headerName: string;
 };
 
 const HeaderFilters = ({ icon, headerName }: THeaderFiltersProps) => {
-	const [value, setValue] = useState("1");
-	const variable = "sgsf";
+	const sortOptions = [
+		{
+			value: SortEnum.dateAsc,
+			icon: AiFillClockCircle,
+			description: "By date Asc",
+		},
+		{
+			value: SortEnum.dateDesc,
+			icon: AiFillClockCircle,
+			description: "By date Desc",
+		},
+		{
+			value: SortEnum.nameAsc,
+			icon: FaArrowDownAZ,
+			description: "By name (A-Z)",
+		},
+		{
+			value: SortEnum.nameDesc,
+			icon: FaArrowUpAZ,
+			description: "By name (Z-A)",
+		},
+		{
+			value: SortEnum.sitesDesc,
+			icon: AiFillChrome,
+			description: "Sites (A-Z)",
+		},
+		{
+			value: SortEnum.sitesAsc,
+			icon: AiFillChrome,
+			description: "Sites (Z-A)",
+		},
+	];
+	const viewOptions = [
+		{
+			value: ViewCollectionsEnum.Card,
+			icon: AiFillAppstore,
+			description: "Card",
+		},
+		{
+			value: ViewCollectionsEnum.List,
+			icon: AiFillProfile,
+			description: "List",
+		},
+		{
+			value: ViewCollectionsEnum.Moodboard,
+			icon: AiFillLayout,
+			description: "Moodboard",
+		},
+	];
+	const ShowInOptions = [
+		{
+			value: ShowInBookmarkEnum.Cover,
+			description: "Cover",
+		},
+		{
+			value: ShowInBookmarkEnum.Title,
+			description: "Title",
+		},
+		{
+			value: ShowInBookmarkEnum.Description,
+			description: "Description",
+		},
+		{
+			value: ShowInBookmarkEnum.BookmarkInfo,
+			description: "Bookmarks info",
+		},
+	];
+	const [sortValueRadio, setSortValueRadio] = useState(sortOptions[0]);
+	const [viewValueRadio, setViewValueRadio] = useState(viewOptions[0]);
+
+	// set default values of inputs in default store managet state
+
+	const onChangeRadioSortOption = (nextValue: string) => {
+		const newSortValue = sortOptions.find(
+			(option) => option.value.toString() === nextValue
+		);
+
+		if (newSortValue) {
+			setSortValueRadio(newSortValue);
+			// set value in store
+		}
+	};
+
+	const onChangeRadioViewOption = (nextValue: string) => {
+		const newViewValue = viewOptions.find(
+			(option) => option.value.toString() === nextValue
+		);
+
+		if (newViewValue) {
+			setViewValueRadio(newViewValue);
+			// set value in store
+		}
+	};
+
+	const onChangeCheckboxShowInOptions = (value: (string | number)[]) => {
+		const checkBoxGroupState = value as string[];
+		console.log(checkBoxGroupState);
+		// set value in store
+	};
+
+	const { getCheckboxProps } = useCheckboxGroup({
+		defaultValue: [
+			ShowInOptions[0]?.value.toString() as string,
+			ShowInOptions[1]?.value.toString() as string,
+			ShowInOptions[3]?.value.toString() as string,
+		],
+		onChange: onChangeCheckboxShowInOptions,
+	});
 
 	return (
 		<Flex>
 			<Flex alignItems="center" gap={3}>
-				<Icon mx="0px" boxSize="3" color="brandPrimary.150" as={AiFillCloud} />
+				{typeof icon === "string" ? (
+					<Image
+						borderRadius="2px"
+						boxSize="5"
+						color="brandPrimary.150"
+						objectFit="contain"
+						src={icon as string}
+						fallbackSrc="/assets/icons/bookmark.svg"
+						alt="General Icon"
+					/>
+				) : (
+					<Icon
+						boxSize="5"
+						color="brandPrimary.150"
+						as={icon as React.ElementType}
+					/>
+				)}
 				<Text textStyle="primary">{headerName}</Text>
 			</Flex>
 
@@ -84,11 +203,11 @@ const HeaderFilters = ({ icon, headerName }: THeaderFiltersProps) => {
 								mx="0px"
 								boxSize="3"
 								color="brandPrimary.150"
-								as={AiFillClockCircle}
+								as={sortValueRadio?.icon}
 							/>
 							<Hide below="md">
 								<Text ml={1} textStyle="secondary" color="brandPrimary.150">
-									{variable}
+									{sortValueRadio?.description}
 								</Text>
 							</Hide>
 						</Button>
@@ -100,60 +219,162 @@ const HeaderFilters = ({ icon, headerName }: THeaderFiltersProps) => {
 						border="1px solid"
 						borderColor="brandPrimary.900"
 						w="100%"
-						p={2}
+						p={1}
 					>
 						<PopoverBody>
-							<RadioGroup onChange={setValue} value={value} defaultValue="2">
+							<RadioGroup
+								onChange={onChangeRadioSortOption}
+								defaultValue={sortValueRadio?.value.toString()}
+							>
 								<Stack direction="column">
-									<Radio
-										value="date-asc"
-										colorScheme="gray"
-										borderColor="brandPrimary.150"
-										_focus={{
-											borderColor: "none",
-											outline: "none",
-											boxShadow: "none",
-										}}
-										_hover={{
-											borderColor: "brandPrimary.100",
-											color: "brandPrimary.150",
-										}}
-									>
-										<Flex align="center" gap={2}>
-											<Icon
-												boxSize="5"
-												color="brandPrimary.100"
-												as={AiFillClockCircle}
-											/>
-											<Text textStyle="primary" color="brandPrimary.100">
-												By date Asc
-											</Text>
-										</Flex>
-									</Radio>
-									<Radio value="date-desc">By date Desc</Radio>
-									<Radio value="name-asc">By name (A-Z)</Radio>
-									<Radio value="name-desc">By name (Z-A)</Radio>
-									<Radio value="sites-asc">Sites (A-Z)</Radio>
-									<Radio value="sites-desc">Sites (Z-A)</Radio>
+									<Text textStyle="primary" color="brandPrimary.100" mb={1}>
+										Sort by
+									</Text>
+									{sortOptions.map((option, index) => {
+										return (
+											<Radio
+												key={`RenderedSortList${index}`}
+												value={option.value.toString()}
+												colorScheme="gray"
+												borderColor="brandPrimary.150"
+												_focus={{
+													borderColor: "none",
+													outline: "none",
+													boxShadow: "none",
+												}}
+												_hover={{
+													borderColor: "brandPrimary.100",
+													color: "brandPrimary.150",
+												}}
+											>
+												<Flex align="center" gap={2}>
+													<Icon
+														boxSize="5"
+														color="brandPrimary.100"
+														as={option.icon}
+													/>
+													<Text textStyle="primary" color="brandPrimary.100">
+														{option.description}
+													</Text>
+												</Flex>
+											</Radio>
+										);
+									})}
 								</Stack>
 							</RadioGroup>
 						</PopoverBody>
 					</PopoverContent>
 				</Popover>
 
-				<Box>
-					<Icon
-						mx="0px"
-						boxSize="3"
-						color="brandPrimary.150"
-						as={AiFillCloud}
-					/>
-					<Hide below="md">
-						<Text ml={1} textStyle="secondary" color="brandPrimary.150">
-							This text hides at the "md" value screen width and smaller.
-						</Text>
-					</Hide>
-				</Box>
+				<Popover>
+					<PopoverTrigger>
+						<Button
+							bg="brandPrimary.900"
+							color="brandPrimary.100"
+							_hover={{
+								bg: "brandPrimary.950",
+							}}
+							_active={{
+								bg: "brandPrimary.950",
+							}}
+						>
+							<Icon
+								mx="0px"
+								boxSize="3"
+								color="brandPrimary.150"
+								as={viewValueRadio?.icon}
+							/>
+							<Hide below="md">
+								<Text ml={1} textStyle="secondary" color="brandPrimary.150">
+									{viewValueRadio?.description}
+								</Text>
+							</Hide>
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent
+						zIndex="popover"
+						bg="brandPrimary.900"
+						color="brandPrimary.100"
+						border="1px solid"
+						borderColor="brandPrimary.900"
+						w="100%"
+						p={1}
+					>
+						<PopoverBody>
+							<RadioGroup
+								onChange={onChangeRadioViewOption}
+								defaultValue={viewValueRadio?.value.toString()}
+							>
+								<Stack direction="column">
+									<Text textStyle="primary" color="brandPrimary.100">
+										Sort by
+									</Text>
+									{viewOptions.map((option, index) => {
+										return (
+											<Radio
+												key={`RenderedViewList${index}`}
+												value={option.value.toString()}
+												colorScheme="gray"
+												borderColor="brandPrimary.150"
+												_focus={{
+													borderColor: "none",
+													outline: "none",
+													boxShadow: "none",
+												}}
+												_hover={{
+													borderColor: "brandPrimary.100",
+													color: "brandPrimary.150",
+												}}
+											>
+												<Flex align="center" gap={2}>
+													<Icon
+														boxSize="5"
+														color="brandPrimary.100"
+														as={option.icon}
+													/>
+													<Text textStyle="primary" color="brandPrimary.100">
+														{option.description}
+													</Text>
+												</Flex>
+											</Radio>
+										);
+									})}
+								</Stack>
+							</RadioGroup>
+
+							<Divider my={3} />
+
+							<Stack direction="column">
+								<Text textStyle="primary" color="brandPrimary.100">
+									Show in list
+								</Text>
+								<Stack spacing={1} direction="column">
+									{ShowInOptions.map((option, index) => {
+										return (
+											<Checkbox
+												key={`RenderedShowInList${index}`}
+												size="sm"
+												value={option.value.toString()}
+												colorScheme="gray"
+												borderColor="brandPrimary.150"
+												_focus={{
+													borderColor: "none",
+													outline: "none",
+													boxShadow: "none",
+												}}
+												{...getCheckboxProps({
+													value: option.value.toString(),
+												})}
+											>
+												{option.description}
+											</Checkbox>
+										);
+									})}
+								</Stack>
+							</Stack>
+						</PopoverBody>
+					</PopoverContent>
+				</Popover>
 			</Flex>
 		</Flex>
 	);
@@ -177,7 +398,7 @@ export const AllBookmarksPage = (): React.ReactElement => {
 
 	return (
 		<>
-			<HeaderFilters headerName="All bookmarks" />
+			<HeaderFilters headerName="All bookmarks" icon={AiFillCloud} />
 			<Stack direction={["column", "row"]} spacing="24px">
 				<Text>All</Text>
 			</Stack>
--- src/WebSPA/src/shared/types/global.types.ts
+++ src/WebSPA/src/shared/types/global.types.ts
@@ -8,8 +8,32 @@ type IconProps = HeroIconSVGProps & {
 
 export type Heroicon = React.FC<IconProps>;
 
+export type EventOrValue = React.ChangeEvent<HTMLInputElement> | string | number;
+
 export enum FormActionEnum {
 	Add = 0,
 	Update = 1,
 	Delete = 2
+}
+
+export enum SortEnum {
+  dateAsc,
+  dateDesc,
+  nameAsc,
+  nameDesc,
+  sitesAsc,
+  sitesDesc
+}
+
+export enum ViewCollectionsEnum {
+  List,
+  Card,
+  Moodboard
+}
+
+export enum ShowInBookmarkEnum {
+  Cover,
+  Title,
+  Description,
+  BookmarkInfo
 }
